2e445b36b4ab11d7e999da285cf72b2d2ebf4752
gthilyer <gthilyer@COE-CS-pc25.OU.AD3.UCDAVIS.EDU>
2022-10-24 17:23:59 -0700

    phase 1

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -5,22 +5,66 @@
 #include "queue.h"
 
 struct queue {
-	/* TODO Phase 1 */
+
+	void* data;  //  holds data a specific memory address
+    queue_t next;  //  points to next structure node in queue
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t head = malloc(sizeof(struct queue));  //  dynamic allocation 
+    head->next = NULL;  //  set both to NULL
+    head->data = NULL;
+
+    return head;
 }
 
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+	 if(queue == NULL){
+        return -1;
+    }
+
+    queue_t freeQueue = NULL;
+    
+    while(queue != NULL){
+        freeQueue = queue;
+        queue = queue->next;
+        free(freeQueue);
+    }
+    freeQueue = NULL;
+    return 0;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+
+	if (queue == NULL || data == NULL) {  //  Error catcher
+		return -1;
+	}
+
+	if (queue->data == NULL) {  //  if first enqueued item
+
+        queue->data = data;  
+        return 0;
+
+    }
+
+    if (queue->next == NULL) {  //  if we have reached end of list 
+
+        queue_t newNode = malloc(sizeof(queue_t));  //  dyanmic allocation 
+        newNode->data = data;  //  point to data passed
+        newNode->next = NULL;  //  SET NEXT TO NULL (critical for segfaults)
+        queue->next = newNode;  //  set new node to null
+        return 0;  //  clean exit value 
+
+    } else {
+
+        return queue_enqueue(queue->next, data);  //  recurse through queue
+    }
+
+	return -1;  //  should never reach this point (if it does, error)
+
 }
 
 int queue_dequeue(queue_t queue, void **data)
